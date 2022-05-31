ae9fa97b8f0394a7a7a77220e6dde5a55f1a99c2
Eric Marquez <eamner@yahoo.com>
2022-05-31 18:31:09 -0400

    Filament admin

--- database/migrations/2022_05_30_225501_create_customers_table.php
+++ database/migrations/2022_05_30_225501_create_customers_table.php
@@ -13,17 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('phone')->nullable();
-            $table->string('address')->nullable();
-            $table->string('facebook_profile')->nullable();
-            $table->string('instagram_profile')->nullable();
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -35,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('customers');
     }
 };
